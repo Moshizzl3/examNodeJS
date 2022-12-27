8e1b39d215a46ed824800090199eb44b1c6b67f6
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-12-27 12:55:22 +0100

    login route

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -1,7 +1,6 @@
 import Router from "express";
 import db from "../database/connection.js";
-import bcrypt from "bcrypt";
-
+import bcrypt, { compare } from "bcrypt";
 
 const router = Router();
 
@@ -10,10 +9,32 @@ router.post("/api/users", async (req, res) => {
   const saltRounds = 12;
 
   user.password = await bcrypt.hash(user.password, saltRounds);
-  await db.execute("INSERT INTO users(first_name, last_name, mail, password, role_id) VALUES(?, ?, ?, ?, ?);",
+  await db.execute(
+    "INSERT INTO users(first_name, last_name, mail, password, role_id) VALUES(?, ?, ?, ?, ?);",
     [user.firstName, user.lastName, user.mail, user.password, 1]
   );
-  res.send({data:"ok"});
+  res.status(200).send({ data: "ok" });
 });
+
+router.post("/login", async (req, res) => {
+  const user = await db.execute("SELECT * FROM users WHERE mail=?", [
+    req.body.mail,
+  ]);
+  if (!user) {
+    return res.status(401).send({
+      succes: false,
+      message: "No user found",
+    });
+  }
+  if(!await bcrypt.compare(req.body.password,user.password)){
+    return res.status(401).send({
+      succes: false,
+      message: "wrong password",
+    });
+  }
+  
+
+});
+
 
 export default router;
