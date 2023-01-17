57b89bd795436c78115b415a12574b5c63894677
Mohamad Ibrahim <mohamedibr@gmail.com>
2023-01-17 21:15:00 +0100

    cleaned frontend code

--- client/src/pages/PageNotFound/PageNotFound.svelte
+++ client/src/pages/PageNotFound/PageNotFound.svelte
@@ -1,3 +1,19 @@
-<div>
-  <h2>Not found</h2>
+<script>
+  import Footer from "../../components/Footer.svelte";
+</script>
+
+<div
+  class="container w-100 h-full md:flex items-start justify-start main-container p-2"
+>
+  <div class="h-full w-full flex justify-center">
+    <h2>Page not found</h2>
+  </div>
 </div>
+
+<Footer />
+
+<style>
+  .main-container {
+    max-height: calc(100vh - 15vh);
+  }
+</style>
